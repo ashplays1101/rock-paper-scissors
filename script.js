ab5904ad5ada0d7f740ffffe8d2d6eb9dc3e1246
Aswin Krishna <aswinkrish.002@gmail.com>
2021-05-21 16:09:06 +0530

    Added user interface for the game. Added stylesheet.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Declaring variables for the player's score, computer's score, and the win status.
-let playerScore = -0;
+let playerScore = 0;
 let computerScore = 0;
 let winStatus = null;
 
@@ -10,8 +10,7 @@ function computerPlay() {
 }
 
 //The function that represents one round of the game.
-function playRound() {
-    let ps = prompt("Type in your choice(Rock, Paper, Scissors)");
+function playRound() {;
     //Both selections are turned into upper case to make it case insensitive.
     let playerSelection = ps.toUpperCase();
     let computerSelection = computerPlay();
@@ -32,26 +31,32 @@ function playRound() {
             break;
         case "ROCK-PAPER":
             winStatus = "win";
+            playerScore++;
             return "You win! Paper beats Rock";
             break;
         case "PAPER-ROCK":
             winStatus = "lose";
+            computerScore++;
             return "You lose! Paper beats Rock";
             break;
         case "ROCK-SCISSORS":
             winStatus = "lose";
+            computerScore++;
             return "You lose! Rock beats Scissors";
             break;
         case "SCISSORS-ROCK":
             winStatus = "win";
+            playerScore++;
             return "You win! Rock beats Scissors";
             break;
         case "PAPER-SCISSORS":
             winStatus = "win";
+            playerScore++
             return "You win! Scissors beat Paper";
             break;
         case "SCISSORS-PAPER":
             winStatus = "lose";
+            computerScore++;
             return "You lose! Scissors beat Paper";
             break;
         default:
@@ -59,41 +64,83 @@ function playRound() {
         }
     }
 
-//This function calls the previous function 5 times, which counts as a single game.
-function game() {
-    //Initialize scores before each game
+//Function to reset the game after one round is over.
+function clearText() {
+    document.getElementById('player').innerText = "0";
+    document.getElementById('computer').innerText = "0";
     playerScore = 0;
     computerScore = 0;
+    winStatus = null;
+    document.getElementById('info').innerText = "";
+    const rock = document.querySelector('#rock');
+    const paper = document.querySelector('#paper');
+    const scissors = document.querySelector('#scissors');
+    rock.setAttribute('onclick', 'rock()');
+    paper.setAttribute('onclick', 'paper()');
+    scissors.setAttribute('onclick', 'scissors()');
+}
 
-    //For loop that calls the playRound function 5 times. Also checks winStatus and increments playerScore or computerScore accordingly.
-    for (var i = 0; i <= 4; i++) {
-        console.log(playRound());
-        if (winStatus === "win") {
-            playerScore++;
-        }
-        else if (winStatus === "lose") {
-            computerScore++;
-        }
-        else {
-            ""
-        }
-    }
+//Function tied to the button corresponding to 'Rock'
+function rock() {
+    ps = 'rock';
+    let result = playRound();
+    document.getElementById('info').innerText = result;
+    document.getElementById('player').innerText = playerScore;
+    document.getElementById('computer').innerText = computerScore;
+    win();
+}
 
-    //If statement chain to determine who won the game and to display the score.
-    if (playerScore > computerScore) {
-        return ("You won! Your score - " + playerScore + ". Computer's score - " + computerScore);
+//Function tied to the button corresponding to 'Paper'
+function paper() {
+    ps = 'paper';
+    let result = playRound();
+    document.getElementById('info').innerText = result;
+    document.getElementById('player').innerText = playerScore;
+    document.getElementById('computer').innerText = computerScore;
+    win();
+}
+
+//Function tied to the button corresponding to 'Scissors'
+function scissors() {
+    ps = 'scissors';
+    let result = playRound();
+    document.getElementById('info').innerText = result;
+    document.getElementById('player').innerText = playerScore;
+    document.getElementById('computer').innerText = computerScore;
+    win();
+}
+
+//Function that is called each time a button is pressed to see if anyone has won
+function win() {
+    if (playerScore === 5 && computerScore === 5) {
+        document.getElementById('info').innerText = "It's a draw! Click the reset button to play again.";
+        disable();
     }
-    else if (playerScore < computerScore) {
-        return ("You lost! Your score - " + playerScore + ". Computer's score - " + computerScore);
+    else if (playerScore === 5 && computerScore < 5) {
+        document.getElementById('info').innerText = "You win! Click the reset button to play again.";
+        disable();
     }
-    else if (playerScore === computerScore) {
-        return ("It's a draw! Your score - " + playerScore + ". Computer's score - " + computerScore);
+    else if (computerScore === 5 && playerScore < 5) {
+        document.getElementById('info').innerText = "You lose! Click the reset button to play again.";
+        disable();
     }
     else {
         return;
     }
 }
 
-//Call the game function and display its output on the console.
-console.log(game());
+//Function to disable button actions once a round is over
+function disable() {
+    const rock = document.querySelector('#rock');
+    const paper = document.querySelector('#paper');
+    const scissors = document.querySelector('#scissors');
+    rock.removeAttribute('onclick');
+    paper.removeAttribute('onclick');
+    scissors.removeAttribute('onclick');
+}
+
+
+
+
+
 
